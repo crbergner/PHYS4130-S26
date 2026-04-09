83bd8ca8b115501082bcc49a279a240105246060
Emma Krebs <114197061+ManlyMouse@users.noreply.github.com>
2026-04-09 13:22:29 -0500

    Still broken
    Need more time to fix structure of octree

--- Emma Krebs/Diffusion/Diffusion_Header.py
+++ Emma Krebs/Diffusion/Diffusion_Header.py
@@ -16,7 +16,7 @@ class Node:
         self.depth = depth
         self.leaf = True
         self.children = [None, None, None, None, None, None, None, None]
-        self.particles = None # Each Node will only hold one particle at a time for its leaves
+        self.particles = [] # Each Node will only hold one particle at a time for its leaves
 
     
 class Particle:
@@ -39,7 +39,7 @@ class Particle:
             self.location[i] += random_walked[i]
 
 
-def subdivide(node, depth_value, center):
+def subdivide(node, depth_value, center, n):
     '''
         Takes a node and subdivides the grid into 8 more parts to create an octree. Quickens computing
         time by limiting the number of possible neighbors a moving particle has to check for sticking. 
@@ -52,21 +52,36 @@ def subdivide(node, depth_value, center):
             None: Updates the nodes directly. Doesn't return anything. 
     '''
 
-    if node.depth < depth_value:
+    if node.depth < n:
         node.leaf = False # It is no longer a leaf case. It has children now!!!
         depth = node.depth + 1 # Update depth
-        grid_value = node.grid[0] / 8
+        grid_value = node.grid[0] / 2
         grid = [grid_value, grid_value, grid_value]
-        for i in range(8):
-            node.children[i] = Node(center/4, grid, depth)
-        index = sort_particles_subdivision(node)
-        node.children[index].particles = node.particles
-        return None
-    else:
-        return None
+        
+        offset_center = [-grid_value/2, grid_value/2] # Need to offset center. Can either add or subtract.
+
+        child_index = 0
+        for dx in offset_center:
+            for dy in offset_center:
+                for dz in offset_center:
+                    center_x = node.center[0] + dx
+                    center_y = node.center[1] + dy
+                    center_z = node.center[2] + dz
+
+                    node.children[child_index] = Node([center_x, center_y, center_z], grid, depth)
+                    child_index += 1
+        
+        old_particles = node.particles
+        node.particles = []
+
+        for p in old_particles:
+            insert_particle(node, p, n)
+
+        for child in node.children:
+            subdivide(child, depth_value, center, n)
 
 
-def generate_nearby_point(point):
+def generate_nearby_point(array):
     '''
         Takes in a given point from the moving particle and returns the 26 directions directly surrounding 
         particle so it can search the nodes of these locations. 
@@ -86,9 +101,9 @@ def generate_nearby_point(point):
                 if i == j == k == 0: # Skip this one because its where the particle is
                     continue
                 else:
-                    grid_x = point[0] + i
-                    grid_y = point[1] + j
-                    grid_z = point[2] + k
+                    grid_x = array[0] + i
+                    grid_y = array[1] + j
+                    grid_z = array[2] + k
 
                     point_grid.append([grid_x, grid_y, grid_z])
 
@@ -111,13 +126,14 @@ def generation_sphere(current_maximum, generation_distance, center):
 
     radius = current_maximum + generation_distance
     u = random.uniform(0,1)
+    v = random.uniform(0,1)
 
     # From a source in document to create a sphere with equally likely random points on it
     theta = 2*np.pi*u
-    phi = np.arccos(1 - 2*u)
-    px = round(np.sin(phi) * np.cos(theta) * radius) + center[0]
-    py = round(np.sin(phi) * np.sin(theta) * radius) + center[1]
-    pz = round(np.cos(phi) * radius) + center[2]
+    phi = np.arccos(1 - 2*v)
+    px = round(np.sin(phi) * np.cos(theta) * radius + center[0])
+    py = round(np.sin(phi) * np.sin(theta) * radius + center[1])
+    pz = round(np.cos(phi) * radius + center[2])
 
     location = [px, py, pz]
 
@@ -147,7 +163,7 @@ def kill_or_be_killed(location, kill_radius, max_distance, center):
         return False
     
 
-def find_node(root, point):
+def find_node(root, point, n):
     '''
         Given a point this finds out with leaf node it is in and returns that node.
 
@@ -156,7 +172,7 @@ def find_node(root, point):
             point (Array): Location of neighborhood. Want to check if there's any particles around.
         
         Returns:
-            locations_of_particle (Array): Returns an array of all nearby particle locations
+            node (Object): Returns node of a certain location
             value (int): Index of child node for point location
     '''
 
@@ -164,37 +180,29 @@ def find_node(root, point):
     # via the formula 4r + 2g + b, but here instead of red, green, and blue we can use 
     # our postive and negative 3 directions. Thus
     node = root
-    value = 0
 
+    value = 0
     # This will loop until it finds the leaf node to extract the particles
     while node.leaf != True:
-
         value = 0
 
-        if  point[0] >= root.center[0]:
+        if  point[0] > node.center[0]:
             value |= 4
-        if point[1] >= root.center[1]:
+        if point[1] > node.center[1]:
             value |= 2
-        if point[2] >= root.center[2]:
+        if point[2] > node.center[2]:
             value |= 1
         
         # Example: Say we said yes to all three if statements. Then we have 7 and that represents
         # our positive quadrant for this center. 
 
         found_node = node.children[value]
-        if found_node.leaf == True:
-            node = found_node
-            break
-        else:
-            node = found_node
 
-    particle_array = node.particles
+        if found_node is None:
+            return node, value
+        node = found_node
 
-    location_of_particles = []
-    for particle in particle_array:
-        location_of_particles.append(particle.location)
-
-    return location_of_particles, value
+    return node, value
 
 
 def stickiness(location_list, particle):
@@ -213,7 +221,7 @@ def stickiness(location_list, particle):
     close_distance = []
     for location in location_list:
         distance = np.sqrt((location[0] - particle.location[0])**2 + (location[1] - particle.location[1])**2 + (location[2] - particle.location[2])**2)
-        if distance <= 1:
+        if distance <= 1.5:
             close_distance.append(location)
 
     # If there was nothing close enough, return false
@@ -222,11 +230,10 @@ def stickiness(location_list, particle):
     else: 
         for close_loc in close_distance:
             prob = particle.probability
-
             if random.uniform(0, 1) <= prob:
                 return True
-            else:
-                return False
+            
+        return False
 
 
 def sort_particles_subdivision(node):
@@ -242,15 +249,68 @@ def sort_particles_subdivision(node):
     # Need to find index, so we will use color method again.
     particle = node.particles
 
-    location = particle.location
+    for particle in node.particles:
+        location = particle.location
 
-    value = 0
+        value = 0
 
-    if location[0] >= node.center[0]:
-            value |= 4
-    if location[1] >= node.center[1]:
-            value |= 2
-    if location[2] >= node.center[2]:
-            value |= 1
+        if location[0] >= node.center[0]:
+                value |= 4
+        if location[1] >= node.center[1]:
+                value |= 2
+        if location[2] >= node.center[2]:
+                value |= 1
 
     return value
+
+
+def insert_particle(node, particle, n):
+    '''
+        Either insert the particle into its node or into a child of that node.
+
+        Args:
+            node (object): The node that is being considered for insertion.
+            particle (object): Particle object from the node that needs to be inserted.
+
+        Returns:
+            None: Simply updates where the particles should be.
+    '''
+    value = 0
+    if node.leaf:
+        if node.depth == n or len(node.particles) == 0:
+            node.particles.append(particle)
+            return None
+        else:
+            old_particle = node.particles[0]
+            node.particles = []
+            subdivide(node, node.depth, node.center, n)
+            insert_particle(node, old_particle, n)
+            insert_particle(node, particle, n)
+            return
+        
+    else: # in the case insertion
+        if particle.location[0] >= node.center[0]:
+            value |= 4
+        if particle.location[1] >= node.center[1]:
+            value |= 2
+        if particle.location[2] >= node.center[2]:
+            value |= 1
+
+        insert_particle(node.children[value], particle, n)
+
+
+def particle_from_center(particle, center):
+    '''
+        Measures particle from center.
+
+        Arg:
+            particle (object): Particle being measured
+            center (array): Center of grid
+
+        Returns: 
+            distance (int): Distance from center
+    '''
+    location = particle.location
+
+    distance = np.sqrt((location[0] - center[0])**2 + (location[1] - center[1])**2 + (location[2] - center[2])**2)
+    return distance
--- Emma Krebs/Diffusion/Main.py
+++ Emma Krebs/Diffusion/Main.py
@@ -15,49 +15,70 @@ generation_distance = 5 # The distance to the surface of a sphere for generating
 kill_distance = 10 # If particle wanders past this point, it is killed
 stuck_particles = [] # The number of stuck particles that appear. Once in order, we can generate them.
 
+print("Done here 0")
+
 # Generate the very first seed and node, known as root node
 root = Diffusion_Header.Node(center, grid, 0)
 seed_particle = Diffusion_Header.Particle(center, probability)
-root.particles = seed_particle
+root.particles.append(seed_particle)
 stuck_particles.append(seed_particle.location)
 
-# Now subdivide it!
-Diffusion_Header.subdivide(root, 0, center)
+print("Done here 1")
+
 node = root # Starting node
 
 # Now start looping through all the particles
-while count < num_particles:
-    
+while len(stuck_particles) < num_particles:
     count += 1
+    
     # Spawn new particle. Start by getting a random location on our generation sphere
     location = Diffusion_Header.generation_sphere(current_maximum, generation_distance, center)
     particle = Diffusion_Header.Particle(location, probability)
 
+    print("Done here 2")
+
+    steps = 0
     while particle.stuck == False:
+        steps += 1
+
+        # If there's too mant steps, stop 
+        if steps > 1000:
+            print("Broke")
+            break 
 
          # Checks to see if particle has gone too far
         test = Diffusion_Header.kill_or_be_killed(particle.location, kill_distance, current_maximum, center)
         
         if test == True:
             count -=1 # Remove the particle
-            del particle # Remove the waste of space...
-            continue
+            break
         else:
             particle.random_walk()
             nearby_points = Diffusion_Header.generate_nearby_point(particle.location)
+            print(nearby_points)
 
             total_location_list = [] # Total list of particles we need to compare with for our point
             for point in nearby_points:
-                location_particles, _ = Diffusion_Header.find_node(root, point)
-                total_location_list += location_particles
+                found_node, _ = Diffusion_Header.find_node(root, point)
+                for p in found_node.particles:
+                    print(p.location)
+                    total_location_list.append(list(p.location))
+            
+            print("Hello")
+
 
             test = Diffusion_Header.stickiness(total_location_list, particle)
             if test == True: # Particle has stuck
                 stuck_particles.append(particle.location)
                 particle.stuck = True
-            
+                node, _ = Diffusion_Header.find_node(root, particle.location)
+        
                 # New particle means we have to check if we can subdivide.
-                _, value = Diffusion_Header.find_node(root, particle.location)
-                # Subdivide the location of the particle
-                Diffusion_Header.subdivide(node.children[value], node.children[value].depth, node.children[value].center)
+                found_node, value = Diffusion_Header.find_node(root, particle.location)
+                found_node.particles.append(particle)
+                
+        if Diffusion_Header.particle_from_center(particle, center) > current_maximum:
+            current_maximum = Diffusion_Header.particle_from_center(particle, center)
+
+# print(stuck_particles)
             
